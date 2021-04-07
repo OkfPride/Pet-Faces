fa5c451dda4849678f736a9cc65428c3898e8d52
Konstantin <Summerparadise938@gmail.com>
2021-04-07 22:12:23 +0300

    init commit

---
+++ src/main/java/com/petsfaces/PetsfacesApplication.java
@@ -0,0 +1,13 @@
+package com.petsfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsfacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsfacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petsfaces/PetsfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petsfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsfacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
